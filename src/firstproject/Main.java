f0d14e042c60c73b829dc8035d5230a682774df2
aki-kata <aki14641@gmail.com>
2025-04-13 01:24:24 +0900

    テーマもダークにできたし、Jar作成もできたし、実行までできちゃったぜ！YEAR

--- src/firstproject/Main.java
+++ src/firstproject/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("これ変更してコミット、プッシュしたらちゃんと反映されるかな？？");
+		System.out.println("テーマもダークにできたし、Jar作成もできたし、実行までできちゃったぜ！YEAR");
 		int a = 5;
 		int b = 4;
 		System.out.println("Sum: " + (a + b));
