fc76d096f80eb122fd95d249504477960315a71a
aki-kata <aki14641@gmail.com>
2025-04-13 00:09:33 +0900

    Initial commit

---
+++ src/firstproject/Main.java
@@ -0,0 +1,14 @@
+package firstproject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello, Eclipse!");
+		int a = 5;
+		int b = 3;
+		System.out.println("Sum: " + (a + b));
+
+	}
+
+}
